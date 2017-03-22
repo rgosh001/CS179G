5ae2ce63d03ad1ad9f3a7f2b02de721000bc1afb
Michael Chen <mchen046@ucr.edu>
2017-03-22 04:13:15 -0700

    switch case for queries

--- server.js
+++ server.js
@@ -17,7 +17,14 @@ const client = new cassandra.Client({
     keyspace: 'hospitals'
 });
 
+const client2 = new cassandra.Client({
+    contactPoints: ['127.0.0.1'],
+    protocolOptions: {port:'9043'},
+    keyspace: 'providers'
+});
+
 client.connect();
+client2.connect();
 
 var server = http.createServer(function (req, res) {
     res.writeHead(200, {
@@ -25,24 +32,61 @@ var server = http.createServer(function (req, res) {
         'Access-Control-Allow-Origin': 'http://localhost:8888',
     });
 
-    var zipcode = req.url.substr(1, req.url.length - 1);
 
-    console.log('before client.execute');
-
-    var row;
-    const query = 'SELECT zipcode, pop, numberofdoctors, ratio FROM pop_doctor_ratio WHERE zipcode = ?';
-    client.execute(query, [zipcode], {prepare: true}).then(function(result) {
-        console.log('after execute and before for');
-        for(var i = 0; i < result.rows.length; i++){
-            row = result.rows[i];
-            console.log('row: ', row);
-        }
-        console.log('finished printing');
-        res.write(JSON.stringify(row));
-        res.end();
-        //console.log('Shutting down');
-        //client.shutdown();
-    })
+    // req.url = /1a&zipcode
+    // zipcode = 1a&zipcode
+    var params = req.url.substr(1, req.url.length - 1);
+    var cases = params.substr(0, 2);
+    var zipcode = params.substr(3, params.length - 1);
+    var amp = zipcode.indexOf('&');
+    var zipcode2 = params.substr(3, amp);
+    var specialty = zipcode.substr(amp + 1, req.url.length - 1);
+    switch(cases) {
+        case '1a':
+            console.log('case: ' + cases);
+            console.log(params);
+            console.log(zipcode);
+            console.log('before client.execute');
+            var row;
+            const query1 = 'SELECT zipcode, pop, numberofdoctors, ratio FROM pop_doctor_ratio WHERE zipcode = ?';
+            client.execute(query1, [zipcode], {prepare: true}).then(function(result) {
+                console.log('after execute and before for');
+                console.log('finished printing');
+                res.write(JSON.stringify(result.rows[0]));
+                res.end();
+            });
+            break;
+        case '1b':
+            console.log('case: ' + cases);
+            console.log(params);
+            console.log(zipcode);
+            console.log('before client.execute');
+            var row;
+            const query2 = 'SELECT zipcode, x, y, hospitalname, providernumber, pop, numberofdoctors, numberofhospitals, ratio FROM pop_doctor_ratio_hospital WHERE zipcode = ?';
+            client.execute(query2, [zipcode], {prepare: true}).then(function(result) {
+                console.log('after execute and before for');
+                console.log('finished printing');
+                res.write(JSON.stringify(result.rows));
+                res.end();
+            });
+            break;
+        case '2a':
+            console.log('case: ' + cases);
+            console.log(zipcode2);
+            console.log(specialty);
+            console.log('before client.execute');
+            var row;
+            const query3 = 'SELECT zipcode, taxonomycode1, count FROM taxonomy_count WHERE zipcode = ? and taxonomycode1 = ?';
+            client2.execute(query3, [zipcode2, specialty], {prepare: true}).then(function(result) {
+                console.log('after execute and before for');
+                console.log('finished printing');
+                res.write(JSON.stringify(result.rows[0]));
+                res.end();
+            });
+            break;
+        default:
+            break;
+    }
 }).listen(8000);
 
 console.log("listening on port 8000")
@@ -51,52 +95,3 @@ server.on('error', function (e) {
     // Handle your error here
     console.log(e);
 });
-
-
-function execQueryOnZipcode(zipcode){
-    const query = 'SELECT zipcode, pop, numberofdoctors, ratio FROM pop_doctor_ratio WHERE zipcode = ?';
-    var row;
-    return new Promise(function(resolve, reject) {
-
-        client.connect()
-            .then(function () {
-                console.log('zipcode: ' + zipcode);
-                return client.execute(query, [zipcode], {prepare: true});
-            })
-            .then(function (result) {
-                for(var i = 0; i < result.rows.length; i++){
-                    row = result.rows[i];
-                    console.log('row: ', row);
-                }
-                //console.log('#rows: ', result.rows.length);
-                //const row = result.rows[0];
-                //console.log('Obtained row: ', row);
-                console.log('Shutting down');
-                client.shutdown();
-
-                //console.log('JSON.stringify(row): ' + JSON.stringify(row));
-                return JSON.stringify(row);
-            })
-            .catch(function (err) {
-                console.error('There was an error when connecting', err);
-                return client.shutdown();
-            });
-
-        if(err){
-            return reject(err);
-        }
-
-        return resolve(result);
-    });
-}
-
-/*client.execute(query) {
-  .then(result => {
-  console.log('hospitalname: %s', result.first().hospitalname);
-  });
-
-client.execute(query, function(err, result) {
-  assert.ifError(err);
-  for(var i = 0; i < result.length; i++){
-  console.log('name: ' + result[i]);
-  }*/
