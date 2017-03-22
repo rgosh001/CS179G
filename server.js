03ca97767c347ba33543f3b1507a01e62f8ee63f
Michael Chen <mchen046@ucr.edu>
2017-03-21 23:25:37 -0700

    testing unit of async query

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const httpProxy = require('http-proxy');
 const cassandra = require('cassandra-driver');
 const async = require('async');
 const assert = require('assert');
+var Promise = require('promise');
+
 
 // connect to the cluster
 const client = new cassandra.Client({
@@ -37,10 +39,36 @@ var server = http.createServer(function (req, res) {
         'Content-Type': 'text/plain' ,
         'Access-Control-Allow-Origin': 'http://localhost:8888',
     });
+
     var zipcode = req.url.substr(1, req.url.length - 1);
-    console.log('execQueryOnZipcode: ' + execQueryOnZipcode(zipcode));
-    res.write(zipcode);
-    res.end();
+
+    client.connect()
+        .then(function () {
+            console.log('zipcode: ' + zipcode);
+        })
+
+    var row;
+    if(1){
+        client.execute(query, [zipcode], {prepare: true});
+        for(var i = 0; i < result.rows.length; i++){
+            row = result.rows[i];
+            console.log('row: ', row);
+        }
+        res.write(JSON.stringify(row));
+        res.end();
+        console.log('Shutting down');
+        client.shutdown();
+    }
+
+
+    /*execQueryOnZipcode(zipcode)
+        .then(result => {
+            res.write(result);
+            res.end();
+        })
+        .catch(err => {
+
+        });*/
 }).listen(8000);
 
 console.log("listening on port 8000")
@@ -54,29 +82,38 @@ server.on('error', function (e) {
 function execQueryOnZipcode(zipcode){
     const query = 'SELECT zipcode, pop, numberofdoctors, ratio FROM pop_doctor_ratio WHERE zipcode = ?';
     var row;
-    client.connect()
-        .then(function () {
-            console.log('zipcode: ' + zipcode);
-            return client.execute(query, [zipcode], {prepare: true});
-        })
-        .then(function (result) {
-            for(var i = 0; i < result.rows.length; i++){
-                row = result.rows[i];
-                console.log('row: ', row);
-            }
-            //console.log('#rows: ', result.rows.length);
-            //const row = result.rows[0];
-            //console.log('Obtained row: ', row);
-            console.log('Shutting down');
-            client.shutdown();
+    return new Promise(function(resolve, reject) {
 
-            //console.log('JSON.stringify(row): ' + JSON.stringify(row));
-            return JSON.stringify(row);
-        })
-        .catch(function (err) {
-            console.error('There was an error when connecting', err);
-            return client.shutdown();
-        });
+        client.connect()
+            .then(function () {
+                console.log('zipcode: ' + zipcode);
+                return client.execute(query, [zipcode], {prepare: true});
+            })
+            .then(function (result) {
+                for(var i = 0; i < result.rows.length; i++){
+                    row = result.rows[i];
+                    console.log('row: ', row);
+                }
+                //console.log('#rows: ', result.rows.length);
+                //const row = result.rows[0];
+                //console.log('Obtained row: ', row);
+                console.log('Shutting down');
+                client.shutdown();
+
+                //console.log('JSON.stringify(row): ' + JSON.stringify(row));
+                return JSON.stringify(row);
+            })
+            .catch(function (err) {
+                console.error('There was an error when connecting', err);
+                return client.shutdown();
+            });
+
+        if(err){
+            return reject(err);
+        }
+
+        return resolve(result);
+    });
 }
 
 /*client.execute(query) {
